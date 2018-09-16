7432d94a2cba7dced3faadb062056cc317dbc083
Kerry Bodle <kerrybodle@yahoo.com>
2018-09-16 17:13:16 -0400

    Add files via upload

--- 3-JavaScript-Basics/04-booleans.js
+++ 3-JavaScript-Basics/04-booleans.js
@@ -16,3 +16,7 @@ console.log("Two is greater than one? " + (2 > 1));
 console.log(3 >= 1);
 
 */
+
+console.log("&& :", 2 === 2 && 1 === 1);
+console.log("|| :", 2 === 2 || 2 === 1);
+console.log("!=", 2 != 1);
