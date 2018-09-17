88fce79edaa7b086f4983823a66a187d15af396b
Kerry Bodle <kerrybodle@yahoo.com>
2018-09-17 08:22:26 -0400

    Add files via upload

--- 3-JavaScript-Basics/06-functions.js
+++ 3-JavaScript-Basics/06-functions.js
@@ -76,3 +76,10 @@ function fullName(first, last) {
     return wholeName;
 }
 fullName("Paul" + " " + "O'Connor");
+
+function petNameAndBreed(name, breed) {
+    var petDetails = name + ", " + breed;
+    console.log(petDetails);
+    return petDetails;
+}
+petNameAndBreed("Sansa", "Generic");
