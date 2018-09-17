df15bb1b5c72996be9760b49aa1414fa2ae082c6
Kerry Bodle <kerrybodle@yahoo.com>
2018-09-17 08:20:53 -0400

    Add files via upload

---
+++ 3-JavaScript-Basics/06-functions.js
@@ -0,0 +1,78 @@
+/*
+
+function hello() {
+    console.log("Hello World!");
+}
+hello();
+
+function pacersWon() {
+    console.log("Pacers won!");
+}
+pacersWon();
+
+pacersWon();
+pacersWon();
+pacersWon();
+pacersWon();
+pacersWon();
+
+function numberEntered(x) {
+    console.log("The number you entered was: ", x);
+}
+numberEntered(5);
+
+function addAnyTwoNumbers(x, y) {
+    console.log(x + y);
+}
+addAnyTwoNumbers(5, 8);
+
+addAnyTwoNumbers(4, 5);
+addAnyTwoNumbers(6, 11);
+addAnyTwoNumbers(7, 8);
+
+function subtractTwoNumbers(firstNum, secondNum) {
+    console.log(secondNum - firstNum);
+}
+subtractTwoNumbers(5, 7);
+
+function getMyTaxReturnAndDoMyTaxesAndStuff(a, x, y, z) {
+    let myInc = a * x;
+    let myTaxes = myInc - y;
+    let total = myTaxes + z;
+    return total;
+}
+getMyTaxReturnAndDoMyTaxesAndStuff(10000, 5, 20, 9);
+*/
+function getMyTaxReturnAndDoMyTaxesAndStuff(a, x, y, z) {
+    let myInc = a * x;
+    let myTaxes = myInc - y;
+    let total = myTaxes + z;
+    console.log(total);
+    return total;
+
+}
+getMyTaxReturnAndDoMyTaxesAndStuff(10000, 20, 5, 0);
+/*
+function add(x, y) {
+    let sum = x + y;
+    console.log(sum);
+    return sum;
+}​
+add(1, 1);
+
+function add(x, y, ) {
+    let sum = x + y;
+    console.log(sum);
+    return sum;
+}
+add(1, 1);
+*/
+
+
+
+function fullName(first, last) {
+    var wholeName = first + " " + last;
+    console.log(wholeName);
+    return wholeName;
+}
+fullName("Paul" + " " + "O'Connor");
