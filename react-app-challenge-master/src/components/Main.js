48bf10a26b2f8227afcadb63980a8fe9c54dbc4b
Kerry Bodle <kerrybodle@yahoo.com>
2018-10-25 11:04:22 -0400

    Add files via upload

---
+++ react-app-challenge-master/src/components/Main.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import HeroButton from './ui/HeroButton';
+import VillainButton from './ui/VillainButton';
+import data from '../data/heroesAndVillans';
+
+
+
+
+
+export default class Main extends Component {
+    constructor() {
+        super()
+        this.state = {
+            mode: '',
+        }
+        this.toggleHero = this.toggleHero.bind(this);
+        this.toggleVillain = this.toggleVillain.bind(this);
+    }
+
+    async toggleHero() {
+        await this.setState({
+            mode: 'hero',
+        })
+        this.mapData();
+    }
+
+    async toggleVillain() {
+        await this.setState({
+            mode: 'villain',
+        })
+        this.mapData();
+    }
+
+    mapData() {
+        let mapData;
+        if (this.state.mode === 'hero') {
+            mapData = data.filter(character => character.hero === true);
+        } else if (this.state.mode === 'villain') {
+            mapData = data.filter(character => character.hero === false);
+        } else {
+            mapData = []
+        }
+
+        let renderData = mapData.map((character, index) => {
+            return (
+                <div key={character.index} className="character-display">
+                    <h2>{character.name}</h2>
+                    <h6>Real Name: {character.realName}</h6>
+                    <p>Universe: {character.universe}</p>
+                    <p>Powers:{character.power}</p>
+                    <p>Heroes or Villains?</p>
+                </div>              
+            )
+        })
+
+        this.setState({
+            renderData: renderData
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <div className="btns" style={leftIndent}>
+                        <HeroButton toggleHero={this.toggleHero} />
+                        <VillainButton toggleVillain={this.toggleVillain} />
+                    </div>
+                </div>
+                <div className="data-display">
+                    {this.state.renderData}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+let leftIndent = {
+    marginLeft: '1em'
+}
